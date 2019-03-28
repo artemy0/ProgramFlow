24c330899ba654595abb21d56bbb997d901c6c16
Artem Gorbachev <gorbachev99artem@gmail.com>
2019-03-28 17:52:21 +0300

    ProgramFlow

---
+++ ProgramFlow/MathOperations.cs
@@ -0,0 +1,44 @@
+﻿namespace ProgramFlow
+{
+    static class MathOperations
+    {
+        public static double Addition(params double[] numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+                result += numbers[i];
+            return result;
+        }
+
+        public static double Subtraction(params double[] numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+                result -= numbers[i];
+            return result;
+        }
+
+        public static double Multiplection(params double[] numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+                result *= numbers[i];
+            return result;
+        }
+
+        public static double Division(params double[] numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+                result /= numbers[i];
+            return result;
+        }
+        
+        public static ulong Factorial(ulong number)
+        {
+            if (number == 1 || number == 0)
+                return 1;
+            return number * Factorial(number - 1);
+        }
+    }
+}
---
+++ ProgramFlow/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace ProgramFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //menu
+                Console.WriteLine("1. + operation");
+                Console.WriteLine("2. - operation");
+                Console.WriteLine("3. * operation");
+                Console.WriteLine("4. / operation");
+                Console.WriteLine("5. ! operation");
+                Console.WriteLine("esc. to exit");
+
+                //select a menu item
+                Console.Write("select: ");
+                ConsoleKeyInfo select = Console.ReadKey(false);
+                Console.WriteLine();
+
+                //exit?
+                if (select.Key == ConsoleKey.Escape)
+                    return;
+
+                //array for the data in which the results will be stored
+                double[] numbers;
+                //calculation of results in accordance with the selected operation
+                double result;
+                switch (select.KeyChar)
+                {
+                    case '1':
+                    case '+':
+                        numbers = ManuallyFillDataArray();
+                        result = MathOperations.Addition(numbers);
+                        PrintOperation(numbers, '+', result);
+                        break;
+                    case '2':
+                    case '-':
+                        numbers = ManuallyFillDataArray();
+                        result = MathOperations.Subtraction(numbers);
+                        PrintOperation(numbers, '-', result);
+                        break;
+                    case '3':
+                    case '*':
+                        numbers = ManuallyFillDataArray();
+                        result = MathOperations.Multiplection(numbers);
+                        PrintOperation(numbers, '*', result);
+                        break;
+                    case '4':
+                    case '/':
+                        numbers = ManuallyFillDataArray();
+                        result = MathOperations.Division(numbers);
+                        PrintOperation(numbers, '/', result);
+                        break;
+                    case '5':
+                    case '!':
+                        Console.Write("enter the number: ");
+                        ulong number = Convert.ToUInt64(Console.ReadLine());
+                        result = MathOperations.Factorial(number);
+                        Console.WriteLine($"Result: {number}! = {result}");
+                        break;
+                    default:
+                        Console.WriteLine("this key doesn't exist.");
+                        break;
+                }
+
+                //Waiting for any key to repeat all actions.
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        //Что делать с данным методом? Лучше ли вывести данные в Main(). Как это сделать?
+        public static double[] ManuallyFillDataArray()
+        {
+            //creating an array for the elements over which the operation will be performed
+            double[] numbers;
+            Console.Write("enter the amount of numbers on which you want to perform the operation: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            numbers = new double[size];
+
+            //input of each element from the keyboard
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"enter {i + 1}th number: ");
+                numbers[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            return numbers;
+        }
+
+        //Что делать с данным методом? Лучше ли вывести данные в Main(). Как это сделать?
+        public static void PrintOperation(double[] numbers, char operationSymbol, double result)
+        {
+            //sample output: Result: 54 + 34 + 8 = 96
+            Console.Write("Result: ");
+            int i;
+            for (i = 0; i < numbers.Length - 1; i++)
+            {
+                Console.Write($"{numbers[i]} {operationSymbol} ");
+            }
+            Console.WriteLine($"{numbers[i]} = {result}");
+        }
+    }
+}
